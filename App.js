d61b9ff82adee396e22f3e5d32ece2c54d3524e5
Consistyandroid <somnath.a.kadam@hotmail.com>
2023-10-06 14:36:39 +0530

    added package .json file

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 // const heading =React.createElement("h1",{id:"heading"},"WELCOME TO React!!!");
 //         const root=ReactDOM.createRoot(document.getElementById("root"));
 //         root.render(heading);
 
 const parent = React.createElement("div", { id: "parent" },
     [React.createElement("div", { id: "child" },
-     [React.createElement("h1", {}, "I am in parent heading 1"),
+     [React.createElement("h1", {}, "I am in parent heading 1 eeee"),
     React.createElement("h2", {}, "I am in parent heading 2"),]),
 
     React.createElement("div", { id: "child2" }, 
